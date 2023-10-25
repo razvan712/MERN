bec6d3ba248d74c956f407d6b72cb9ba20e2f200
razvan712 <111775069+razvan712@users.noreply.github.com>
2023-10-25 20:10:30 +0300

    adn promis

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ mongoose.connect(dbURL,
     useNewUrlParser: true,  
     useUnifiedTopology: true,   
   } 
-);   
+).then(() => console.log("Connected to MongoDB")).catch(e=>console.log(e)) 
  
 app.listen(PORT, ()=> console.log("Server started at port 3001"));  
