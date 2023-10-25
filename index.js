1cd4409145a40322306772a16ac01b71e7f56cd1
razvan712 <111775069+razvan712@users.noreply.github.com>
2023-10-25 19:29:30 +0300

    change path

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { userRouter } from "./routes/users.js";
-import { recipesRouter } from "./routes/recipes.js"; 
+import { userRouter } from "./src/routes/users.js";
+import { recipesRouter } from "./src/routes/recipes.js"; 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
